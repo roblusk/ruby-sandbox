ed3df5ee590a19acecea43d805eb34827b366f48
roblusk <130242918+roblusk@users.noreply.github.com>
2024-04-09 20:31:30 +0000

    initial commit

---
+++ debugger.rb
@@ -0,0 +1,11 @@
+
+
+require "pry-byebug"
+
+f = "Your lucky number is "
+
+l = rand(100)
+
+byebug
+
+pp f + l
---
+++ howdy.rb
@@ -0,0 +1,7 @@
+my_string = "hello, world"
+
+pp "Enter your name:"
+
+their_name = gets.chomp
+
+puts "Hello, " + their_name + "!"
